5d9845860ef534262502eb4f2b1f473ad925165e
AbdulNasirQader <nasakanasir1@gmail.com>
2023-03-30 07:54:48 -0400

    issues in sun still resolving

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         solar.planets="2";
         solar.moon="2";
         solar.sun="1";
+
+        Sun fero= new Sun();
+        fero.color="red";
+        fero.temperature=34234;
+        fero.heat="3243243243k";
     }
 }
