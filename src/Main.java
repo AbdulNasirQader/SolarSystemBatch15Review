dd196f4e07148bce0a119ee1d4f1411ce4d28ce1
AbdulNasirQader <nasakanasir1@gmail.com>
2023-03-30 07:23:17 -0400

    the basic of solar system is ready and deployed

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem solar= new SolarSystem();
+        solar.planets="2";
+        solar.moon="2";
+        solar.sun="1";
+    }
+}
